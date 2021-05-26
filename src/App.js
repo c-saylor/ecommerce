02216ecb39bdfa66b2ccc4696b5bf7452b991e4c
c-saylor <christina.renee106@gmail.com>
2021-05-26 13:04:41 -0400

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
+import {Route} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
