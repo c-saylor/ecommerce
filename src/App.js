758a5304b0cbc7a6d8dacce0fcfb3fd7d6f8ea0a
c-saylor <christina.renee106@gmail.com>
2021-05-26 14:00:53 -0400

    update app with shop page, collection preview component and collection item

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component.jsx';
-import {Route} from 'react-router-dom';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import ShopPage from './pages/shop/shop.component.jsx';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+      </Switch>
     </div>
   );
 }
